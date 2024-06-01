3c69c5174de53859d2f0b27a75258ac2c184dd81
ufofon <ufofon@gmail.com>
2024-06-01 17:58:23 +0900

    ok44

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Positive;
 public record Book (
 
 
-
+       
 
     @NotBlank(message = "The book ISBN must be defined.")
     @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
