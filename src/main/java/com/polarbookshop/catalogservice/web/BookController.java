6df1acd06b0ceee556bc8320d10bad8d8c47eca7
ufofon <ufofon@gmail.com>
2024-06-01 17:54:00 +0900

    ok22

--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -22,6 +22,8 @@ public class BookController {
 
     private final BookService bookService;
 
+      
+
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
